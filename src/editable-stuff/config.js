d92e31b347246a1cb3d70bd75e6b64c6b517ed5e
Edward Chang <eddiechang96@gmail.com>
2021-09-10 12:27:40 -0400

    changes

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,31 +6,28 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Edward",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Chang",
+  message: " Building a better world with data and technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/edwardhchang",
     },
     {
       image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://www.facebook.com/greaterloser",
     },
     {
       image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
+      url: "https://www.instagram.com/eddiecgang/",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/edwardhchang/",
     },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
+    ,
   ],
 };
 
@@ -51,11 +48,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/eddie.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "Hi, my name is Eddie! I am a detail-oriented, analytical, and adaptable data scientist, interested in using data-driven approaches from a top-down perspective to create solutions for global problems. Having grown up in a small-business family and having worked in business financing at PayPal, I find data can drive improved business decisions and uncover areas of improved efficiency. Meticulous in many aspects when it comes to deliverables, I aim to find simple answers to multi-tiered problems and have a proven track record of doing so in high-pressure environments. As a recent graduate of General Assembly's Immersive Data Science camp, I am seeking a role with an organization where I can  collaborate with a team on difficult problems to provide simple and effective results to clients.",
+  resume: "https://docs.google.com/document/d/17-yKoPf2iqZ378RJFpgGVPu49nJOU3j1/edit?usp=sharing&ouid=107353429405971597705&rtpof=true&sd=true",
 };
 
 // PROJECTS SECTION
@@ -66,9 +63,9 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
-  specificRepos: [],
+  gitHubUsername: "edwardhchang", //i.e."johnDoe12Gh"
+  reposLength: 0,
+  specificRepos: ['Loan-Repayment-Classification', 'Alaska-Sea-Level-Prediction', 'home'],
 };
 
 // Leadership SECTION
@@ -78,15 +75,15 @@ const leadership = {
   message:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    {
+      img: require("../editable-stuff/eddie.png"),
+      label: "First slide label",
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
     },
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    {
+      img: require("../editable-stuff/eddie.png"),
+      label: "Second slide label",
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
     },
   ],
   imageSize: {
@@ -100,24 +97,24 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "Python", value: 85 },
+    { name: "SQL", value: 50 },
+    { name: "TensorFlow", value: 75 },
+    { name: "NLP", value: 60 },
+    { name: "Scikit-Learn", value: 80 },
+    { name: "Matplotlib", value: 70 },
+    { name: "PySpark", value: 55 },
+    { name: "R", value: 40 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
     { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
-    { name: "Adaptability", value: 85 },
+    { name: "Positivity", value: 85 },
+    { name: "Adaptability", value: 95 },
     { name: "Problem Solving", value: 75 },
     { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
+    { name: "Organization", value: 80 },
+    { name: "Creativity", value: 70 },
   ],
 };
 
@@ -126,23 +123,23 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for full-time Data Scientist, Data Engineering, or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "eddiechang96@gmail.com",
 };
 
 const experiences = {
-  show: false,
+  show: true,
   heading: "Experiences",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      role: 'Data Science Fellow',// Here Add Company Name
+      companylogo: require('../assets/img/ga.png'),
+      date: 'June 2021 – September 2021',
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      role: 'Senior Credit Underwriter',// Here Add Company Name
+      companylogo: require('../assets/img/paypal.png'),
+      date: 'February 2018 – May 2021',
     },
   ]
 }
