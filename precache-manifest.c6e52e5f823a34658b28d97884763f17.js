4bad84c326a2ffc5fdf164144c0f98add3a14bde
Edward Chang <eddiechang96@gmail.com>
2021-09-10 12:26:29 -0400

    Updates

---
+++ precache-manifest.c6e52e5f823a34658b28d97884763f17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a83cab7bd0f9cf7c3a47c2baba27a4b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "f23cd1baad5656b6975b",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23cd1baad5656b6975b",
+    "url": "/home/static/js/main.65c82521.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "e957b24b5f4894673d928fd94f652f8c",
+    "url": "/home/static/media/eddie.e957b24b.png"
+  },
+  {
+    "revision": "7cd7cfea57eb89b505830ef934c7f9d3",
+    "url": "/home/static/media/paypal.7cd7cfea.png"
+  },
+  {
+    "revision": "ed2ce183142c0af87a7cd2009a449692",
+    "url": "/home/static/media/resume.ed2ce183.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.c6e52e5f823a34658b28d97884763f17.js"
 );
 
 self.addEventListener('message', (event) => {
